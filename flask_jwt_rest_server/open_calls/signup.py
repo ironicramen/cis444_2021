052feef5e8482bda0a889fe523440a223fcc16bd
ironicramen <1keithgross@gmail.com>
2021-11-22 07:54:37 +0000

    some fixes

--- flask_jwt_rest_server/open_calls/signup.py
+++ flask_jwt_rest_server/open_calls/signup.py
@@ -1,6 +1,5 @@
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
-#from tools.token_tools import create_token
 from psycopg2 import sql
 import bcrypt
 
--- flask_jwt_rest_server/secure_calls/get_books.py
+++ flask_jwt_rest_server/secure_calls/get_books.py
@@ -1,11 +1,18 @@
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from tools.token_tools import create_token
-
+from psycopg2 import sql
 from tools.logging import logger
+
+cur = g.db.cursor()
 
 def handle_request():
     logger.debug("Get Books Handle Request")
 
-    return json_response( token = create_token(  g.jwt_data ) , books = {})
+    bookQuery = sql.SQL("SELECT * FROM {table}").format(
+        table = sql.Identifier('books'))
+
+    cur.execute(bookQuery)
+    bookQueryResult = cur.fetchall()
+    return json_response( token = create_token(  g.jwt_data ) , books = bookQueryResult)
 
